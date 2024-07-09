caa28d2c043fb0e2c03809250fa29f8a11098213
hkyuuu00 <lhk3121@gmail.com>
2024-07-09 13:19:39 +0900

    home 배너 추가

--- navi-client/src/components/Service/Home.js
+++ navi-client/src/components/Service/Home.js
@@ -3,13 +3,18 @@ import Header from "./Header";
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 import "../../style/Service/Home.scss";
+import InfiniteSlider from "../ETC/InfiniteSlider";
+
 
 function Home({ isDarkMode, toggleDarkMode }) {
+
+
   return (
     <div>
       <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>
-      <h1>Home</h1>
+      
       <Link to="/Navi"><button className='homeBtn'>시작하기</button></Link>
+      <InfiniteSlider isDarkMode={isDarkMode}/>
       <Footer isDarkMode={isDarkMode}/>
     </div>
   )
